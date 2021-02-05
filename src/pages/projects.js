6f0ab92942998aeb0df9f936811015effffa2c9b
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-04 22:45:23 -0500

    Start Projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const projects = () => (
+  <Layout>
+    <div id="main">
+      <h1>Projects</h1>
+      <form>Search bar</form>
+    </div>
+  </Layout>
+)
+
+export default projects
