be14286ac95bde3f0dbcd76ef9b79eb04a9b8185
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-09 21:15:31 -0500

    wip project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+const finishedProjects = {
+  "001": {
+    id: '001',
+    name: 'Tiny App1',
+    desc: 'URL shortening App',
+    stack: ['HTML', 'CSS', 'EJS',],
+  },
+  "002": {
+    id: '002',
+    name: 'Tiny App2',
+    desc: 'URL shortening App',
+    stack: ['HTML', 'CSS', 'EJS',],
+  },
+  "003": {
+    id: '003',
+    name: 'Tiny App3',
+    desc: 'URL shortening App',
+    stack: ['HTML', 'CSS', 'EJS',],
+  }
+}
+
+const parseList = Object.values(finishedProjects).map((project) => {
+  return (
+    <li>{project.name}</li>
+  )
+})
+
 const projects = () => (
   <Layout>
     <div id="main">
       <h1>Projects</h1>
       <form>Search bar</form>
+      <ul>{parseList ? parseList : "No Items"}</ul>
     </div>
   </Layout>
 )
