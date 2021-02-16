ab2410b2d6a55158ea8837cc51175fde5bef38e7
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-16 14:30:20 -0500

    Add handleChange to Search form

--- src/components/Gallery/components/Gallery.js
+++ src/components/Gallery/components/Gallery.js
@@ -5,20 +5,25 @@ import GalleryItem from './GalleryItem'
 import { projects } from '../constants/projects'
 
 const Gallery = ({ images = projects }) => {
-  const [lightboxIsOpen, setLightboxIsOpen] = useState(false)
-  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [form, setForm] = useState("");
 
   const toggleLightbox = useCallback(selectedIndex => {
     setLightboxIsOpen(!lightboxIsOpen)
     setSelectedIndex(selectedIndex)
-  }, [lightboxIsOpen])
+  }, [lightboxIsOpen]);
 
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setForm(value);
+  };
 
 
   return (
     <div>
       <form>
-        <input type="text" id="projects" name="projects" value="" placeholder="Search Projects ie 'React' 'Javascript'"></input>
+        <input type="text" id="projects" name="projects" onChange={handleChange} value={form} placeholder="Search Projects..."></input>
       </form>
       {images && (<div className="row">
         {images.map((obj, i) => {
