f4687f424014a2a20aecfbb527c37278c8e98eaa
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-23 13:59:20 -0500

    Fix constant import names

--- src/components/Gallery/constants/projects.js
+++ src/components/Gallery/constants/projects.js
@@ -1,22 +1,22 @@
-import full01 from '../../../assets/images/fulls/01.jpg'
-import full02 from '../../../assets/images/fulls/02.jpg'
-import full03 from '../../../assets/images/fulls/03.gif'
-import full04 from '../../../assets/images/fulls/04.gif'
-import full05 from '../../../assets/images/fulls/05.jpg'
-import full06 from '../../../assets/images/fulls/06.jpg'
-import thumb01 from '../../../assets/images/thumbs/01.jpg'
-import thumb02 from '../../../assets/images/thumbs/02.jpg'
-import thumb03 from '../../../assets/images/thumbs/03.jpg'
-import thumb04 from '../../../assets/images/thumbs/04.jpg'
-import thumb05 from '../../../assets/images/thumbs/05.jpg'
-import thumb06 from '../../../assets/images/thumbs/06.jpg'
+import bookEndsFull from '../../../assets/images/fulls/01.jpg'
+import keeprFull from '../../../assets/images/fulls/02.jpg'
+import schedulerFull from '../../../assets/images/fulls/03.gif'
+import jungleFull from '../../../assets/images/fulls/04.gif'
+import tweeterFull from '../../../assets/images/fulls/05.jpg'
+import tinyAppFull from '../../../assets/images/fulls/06.jpg'
+import bookEndsThumb from '../../../assets/images/thumbs/01.jpg'
+import keeprThumb from '../../../assets/images/thumbs/02.jpg'
+import schedulerThumb from '../../../assets/images/thumbs/03.jpg'
+import jungleThumb from '../../../assets/images/thumbs/04.jpg'
+import tweeterThumb from '../../../assets/images/thumbs/05.jpg'
+import tinyAppThumb from '../../../assets/images/thumbs/06.jpg'
 
 
 export const projects = [
   {
     id: '1',
-    source: full01,
-    thumbnail: thumb01,
+    source: bookEndsFull,
+    thumbnail: bookEndsThumb,
     caption: 'BookEnds',
     description: "Book data manager, allowing users to add data about the books they've read and share with friends. Group Collaboration",
     gitub: `https://github.com/dimontejulia/bookends`,
@@ -25,8 +25,8 @@ export const projects = [
   },
   {
     id: '2',
-    source: full02,
-    thumbnail: thumb02,
+    source: keeprFull,
+    thumbnail: keeprThumb,
     caption: 'PasswordKeepr',
     description: 'Password generator and manager ideal for shared accounts.  Built using NodeJS, Express, EJS, and CSS. Group Collaboration',
     gitub: `https://github.com/dlinardi/PasswordKeepR`,
@@ -35,8 +35,8 @@ export const projects = [
   },
   {
     id: '3',
-    source: full03,
-    thumbnail: thumb03,
+    source: schedulerFull,
+    thumbnail: schedulerThumb,
     caption: 'Scheduler',
     description: 'React based Interview Scheduler (SPA), built using TDD methods.',
     gitub: `https://github.com/CharlesP8412/scheduler`,
@@ -45,8 +45,8 @@ export const projects = [
   },
   {
     id: '4',
-    source: full04,
-    thumbnail: thumb04,
+    source: jungleFull,
+    thumbnail: jungleThumb,
     caption: 'Jungle',
     description: 'Mini E-commerce app built using MVC design, integrates Stripe payments API. Build using EDD.',
     gitub: `https://github.com/CharlesP8412/jungle`,
@@ -55,23 +55,22 @@ export const projects = [
   },
   {
     id: '5',
-    source: full05,
-    thumbnail: thumb05,
+    source: tweeterFull,
+    thumbnail: tweeterThumb,
     caption: 'Tweeter',
     description: 'Single Page Application, Twitter Clone utilizing client side rendering.',
     gitub: `https://github.com/CharlesP8412/tweeter`,
-    stack: ['NodeJS', 'jQuery', 'HTML', 'CSS',],
+    stack: ['jQuery', 'NodeJS', 'HTML', 'CSS',],
     features: ['Single Page App (SPA)', 'Client Side Rendering',]
   },
   {
     id: '6',
-    source: full06,
-    thumbnail: thumb06,
+    source: tinyAppFull,
+    thumbnail: tinyAppThumb,
     caption: 'Tiny App',
     description: 'URL Shortening App. Given a url the app will generate a shorter url and act as a relay. (Server-side rendering)',
     gitub: `https://github.com/CharlesP8412/tinyapp`,
-    stack: ['NodeJS', 'Bootstrap', 'HTML', 'CSS', 'Mocha', 'Chai'],
+    stack: ['Bootstrap', 'NodeJS', 'HTML', 'CSS', 'Mocha', 'Chai'],
     features: ['Server Side Rendering', 'User Authentication', 'bcrypt', 'Cookie-sessions']
   },
-
 ]
