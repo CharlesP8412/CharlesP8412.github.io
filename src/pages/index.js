e75931e08e4c6f47e1b7b83875af94879feed1a4
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-03-12 08:54:08 -0500

    Add Article Tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Gallery from '../components/Gallery'
 const HomeIndex = () => {
   const [showScroll, setShowScroll] = useState(false)
   const siteTitle = 'Chuck Prystupa - Web Developer'
-  const siteDescription = 'Portfolio of Chuck Prystupa'
+  const siteDescription = 'Portfolio of Chuck Prystupa, full-stack web developer with a background in IT.  '
 
   const scrollTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -36,45 +36,48 @@ const HomeIndex = () => {
 
       <div id="main">
         <section id="one">
-          <header className="major">
-            <h2>
-              Developer, IT Admin, Communications Specialist,&nbsp;
+          <article>
+            <header className="major">
+              <h2>
+                Developer, IT Admin, Communications Specialist,&nbsp;
               <br />
               Adaptable Problem Solver
             </h2>
-          </header>
-          <p>
-            As a full-stack developer, I truly enjoy having the ability to shape and sculpt
-            the nerves of the information systems we use every day.
+            </header>
+            <p>
+              As a full-stack developer, I truly enjoy having the ability to shape and sculpt
+              the nerves of the information systems we use every day.
             <br />
             I've always gravitated to electronics and technology throughout my life.
             From developing and experimenting with my home lab, to deploying and using radio and information systems in past careers,
             I am always keen to develop new skills and apply my experience and prior skills to find new solutions.
           </p>
-
-          <p>
-            <strong>Languages:</strong> <br />
+          </article>
+          <article>
+            <p>
+              <strong>Languages:</strong> <br />
             Javascript, Ruby, HTML, CSS, SQL  <br />
-            <strong>Frameworks:</strong> <br />
+              <strong>Frameworks:</strong> <br />
             React, SASS, Express, Axios, jQuery, EJS, Rails, NodeJS  <br />
-            <strong>Databases and CMS:</strong> <br />
+              <strong>Databases and CMS:</strong> <br />
             PostgreSQL (PSQL), Git, GitLab, Github, Jenkins  <br />
-            <strong>Testing, CD/CI:</strong> <br />
+              <strong>Testing, CD/CI:</strong> <br />
             JEST, Storybook, Cypress, Mocha, Chai, Rspec, CircleCI, Jenkins  <br />
-            <strong>Other Skills:</strong> <br />
+              <strong>Other Skills:</strong> <br />
             Networking, Server, and Desktop Administration, Linux, Project Management
           </p>
-
-          <p>
-            <strong>Education:</strong> <br />
-            <strong>Lighthouse Labs <br /> Web Development - Jan 2021</strong> <br />
+          </article>
+          <article>
+            <p>
+              <strong>Education:</strong> <br />
+              <strong>Lighthouse Labs <br /> Web Development - Jan 2021</strong> <br />
             12-week boot camp, teaching JavaScript, Ruby on Rails, React JS, Database and Data Modeling, Automated Testing, and Computer Science Fundamentals
             <br />
-            <strong>Red River College <br /> Advanced Network Technology - May 2005</strong> <br />
+              <strong>Red River College <br /> Advanced Network Technology - May 2005</strong> <br />
             Network Administration  (CCNP Level), Microsoft Server Administration, Unix, and Wireless Networks  <br />
             Graduated with Dean's Honours
           </p>
-
+          </article>
           <ul className="actions">
             <li>
               <span>
@@ -101,12 +104,14 @@ const HomeIndex = () => {
         </section>
 
         <section id="three">
-          <h2>Get In Touch</h2>
-          <p>
-            If you are looking for talent for your company or organization
-            please don't hesitate to contact me.
-            I would enjoy discussing how I can contribute to your organization.
+          <article>
+            <h2>Get In Touch</h2>
+            <p>
+              If you are looking for talent for your company or organization
+              please don't hesitate to contact me.
+              I would enjoy discussing how I can contribute to your organization.
           </p>
+          </article>
           <div className="row">
             <div className="4u 12u$(small)">
               <ul className="labeled-icons">
